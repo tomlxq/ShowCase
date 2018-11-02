083935cd060e4ac46ef76c8079ab62cf6f69f627
tomluo <beauty2001@163.com>
2018-11-02 23:00:13 +0800

    update cglib

---
+++ spring-base/src/test/java/com/example/demo/proxy/cglib/TomCglibProxy.java
@@ -0,0 +1,25 @@
+package com.example.demo.proxy.cglib;
+
+
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class TomCglibProxy implements MethodInterceptor {
+    public Object getInstance(Object o){
+        Enhancer enhancer=new Enhancer();
+        enhancer.setSuperclass(o.getClass());
+         enhancer.setCallback(this);
+         return enhancer.create();
+    }
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("我是Cglib媒婆");
+        Object oo=methodProxy.invokeSuper(o,objects);
+        System.out.println("我是Cglib媒婆，帮找到爱");
+        return oo;
+    }
+};
+
--- spring-base/src/test/java/com/example/demo/proxy/jdk/TomInvocationHandler.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.proxy.jdk;
-
-import java.lang.reflect.Method;
-
-public interface  TomInvocationHandler {
-      Object invoke(Object proxy, Method method, Object[] args) throws Throwable;
-}
